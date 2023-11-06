be34b1f927a6c5c25fa6c8f02c356c915f3512d6
hxg-avensys <H.Xiang@aven-sys.com>
2023-11-06 18:03:36 +0800

    Fix indentation 2

--- src/main/java/com/avensys/rts/userservice/controller/UserController.java
+++ src/main/java/com/avensys/rts/userservice/controller/UserController.java
@@ -34,151 +34,151 @@ import com.avensys.rts.userservice.util.ResponseUtil;
 @RequestMapping("/api/user")
 public class UserController {
 
-            @Autowired
-            private UserService userService;
+        @Autowired
+        private UserService userService;
 
-            @Autowired
-            private AuthenticationManager authenticationManager;
+        @Autowired
+        private AuthenticationManager authenticationManager;
 
-            @Autowired
-            private MessageSource messageSource;
+        @Autowired
+        private MessageSource messageSource;
 
-            @PostMapping("/signin")
-            public ResponseEntity<?> authenticateUser(@RequestBody LoginDTO loginDTO) {
-                Authentication authenticate = authenticationManager
-                        .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
-                try {
-                    if (authenticate.isAuthenticated()) {
-                        LoginResponseDTO response = userService.login(loginDTO);
+        @PostMapping("/signin")
+        public ResponseEntity<?> authenticateUser(@RequestBody LoginDTO loginDTO) {
+            Authentication authenticate = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
+            try {
+                if (authenticate.isAuthenticated()) {
+                    LoginResponseDTO response = userService.login(loginDTO);
 
-                        return new ResponseEntity<>(response, HttpStatus.OK);
-                    } else {
-                        return ResponseUtil.generateSuccessResponse(null, HttpStatus.UNAUTHORIZED, messageSource.getMessage(
-                                MessageConstants.ERROR_USER_EMAIL_NOT_FOUND, null, LocaleContextHolder.getLocale()));
-                    }
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.UNAUTHORIZED, messageSource
-                            .getMessage(MessageConstants.ERROR_USER_EMAIL_NOT_FOUND, null, LocaleContextHolder.getLocale()));
+                    return new ResponseEntity<>(response, HttpStatus.OK);
+                } else {
+                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.UNAUTHORIZED, messageSource.getMessage(
+                            MessageConstants.ERROR_USER_EMAIL_NOT_FOUND, null, LocaleContextHolder.getLocale()));
                 }
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.UNAUTHORIZED, messageSource
+                        .getMessage(MessageConstants.ERROR_USER_EMAIL_NOT_FOUND, null, LocaleContextHolder.getLocale()));
             }
+        }
 
-            @PostMapping("/signup")
-            public ResponseEntity<?> registerUser(@RequestBody UserEntity user) {
-                try {
-                    // create user object
-                    userService.saveUser(user);
+        @PostMapping("/signup")
+        public ResponseEntity<?> registerUser(@RequestBody UserEntity user) {
+            try {
+                // create user object
+                userService.saveUser(user);
 
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.CREATED,
-                            messageSource.getMessage(MessageConstants.USER_REGISTERED, null, LocaleContextHolder.getLocale()));
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.BAD_REQUEST, messageSource
-                            .getMessage(MessageConstants.ERROR_EMAIL_TAKEN, null, LocaleContextHolder.getLocale()));
-                }
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.CREATED,
+                        messageSource.getMessage(MessageConstants.USER_REGISTERED, null, LocaleContextHolder.getLocale()));
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.BAD_REQUEST, messageSource
+                        .getMessage(MessageConstants.ERROR_EMAIL_TAKEN, null, LocaleContextHolder.getLocale()));
             }
+        }
 
-            @GetMapping("/logout")
-            public ResponseEntity<LogoutResponseDTO> logout(@RequestParam("token") String token) {
-                LogoutResponseDTO logoutResponseDTO = userService.logout(token);
-                return ResponseEntity.ok(logoutResponseDTO);
+        @GetMapping("/logout")
+        public ResponseEntity<LogoutResponseDTO> logout(@RequestParam("token") String token) {
+            LogoutResponseDTO logoutResponseDTO = userService.logout(token);
+            return ResponseEntity.ok(logoutResponseDTO);
+        }
+
+        @GetMapping("/validate")
+        public ResponseEntity<InstrospectResponseDTO> validate(@RequestParam("token") String token) {
+            InstrospectResponseDTO instrospectResponseDTO = userService.validate(token);
+            return ResponseEntity.ok(instrospectResponseDTO);
+        }
+
+        @PostMapping
+        public ResponseEntity<?> createUser(@RequestBody UserEntity user) {
+            try {
+                userService.saveUser(user);
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.CREATED,
+                        messageSource.getMessage(MessageConstants.USER_CREATED, null, LocaleContextHolder.getLocale()));
+
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.BAD_REQUEST, e.getMessage());
             }
+        }
 
-            @GetMapping("/validate")
-            public ResponseEntity<InstrospectResponseDTO> validate(@RequestParam("token") String token) {
-                InstrospectResponseDTO instrospectResponseDTO = userService.validate(token);
-                return ResponseEntity.ok(instrospectResponseDTO);
+        @PutMapping
+        public ResponseEntity<?> editUser(@RequestBody UserEntity user) {
+            try {
+                userService.update(user);
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.OK,
+                        messageSource.getMessage(MessageConstants.USER_UPDATED, null, LocaleContextHolder.getLocale()));
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
             }
+        }
 
-            @PostMapping
-            public ResponseEntity<?> createUser(@RequestBody UserEntity user) {
-                try {
-                    userService.saveUser(user);
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.CREATED,
-                            messageSource.getMessage(MessageConstants.USER_CREATED, null, LocaleContextHolder.getLocale()));
-
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.BAD_REQUEST, e.getMessage());
-                }
+        @DeleteMapping("/{id}")
+        public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
+            try {
+                userService.delete(id);
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.OK,
+                        messageSource.getMessage(MessageConstants.USER_DELETED, null, LocaleContextHolder.getLocale()));
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
             }
+        }
 
-            @PutMapping
-            public ResponseEntity<?> editUser(@RequestBody UserEntity user) {
-                try {
-                    userService.update(user);
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.OK,
-                            messageSource.getMessage(MessageConstants.USER_UPDATED, null, LocaleContextHolder.getLocale()));
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
-                }
-            }
-
-            @DeleteMapping("/{id}")
-            public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
-                try {
-                    userService.delete(id);
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.OK,
-                            messageSource.getMessage(MessageConstants.USER_DELETED, null, LocaleContextHolder.getLocale()));
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
-                }
-            }
-
-            @GetMapping("/{id}")
-            public ResponseEntity<?> find(@PathVariable("id") Long id) {
-                try {
-                    UserEntity user = userService.getUserById(id);
-                    return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntitytoResponse(user), HttpStatus.OK,
-                            null);
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
-                }
-            }
-
-            @GetMapping
-            public ResponseEntity<?> findAll() {
-                List<UserEntity> users = userService.fetchList();
-                return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntityListtoResponse(users), HttpStatus.OK,
+        @GetMapping("/{id}")
+        public ResponseEntity<?> find(@PathVariable("id") Long id) {
+            try {
+                UserEntity user = userService.getUserById(id);
+                return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntitytoResponse(user), HttpStatus.OK,
                         null);
-
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
             }
+        }
 
-            @GetMapping("/email/{email}")
-            public ResponseEntity<Object> getUserByEmail(@PathVariable String email) {
-                try {
-                    UserEntity user = userService.getUserByEmail(email);
-                    return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntitytoResponse(user), HttpStatus.OK,
-                            null);
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
-                }
+        @GetMapping
+        public ResponseEntity<?> findAll() {
+            List<UserEntity> users = userService.fetchList();
+            return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntityListtoResponse(users), HttpStatus.OK,
+                    null);
+
+        }
+
+        @GetMapping("/email/{email}")
+        public ResponseEntity<Object> getUserByEmail(@PathVariable String email) {
+            try {
+                UserEntity user = userService.getUserByEmail(email);
+                return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntitytoResponse(user), HttpStatus.OK,
+                        null);
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
             }
+        }
 
-            @GetMapping("/profile")
-            public ResponseEntity<Object> getUserDetail() {
+        @GetMapping("/profile")
+        public ResponseEntity<Object> getUserDetail() {
 
-                try {
-                    UserEntity user = userService.getUserDetail();
-                    return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntitytoResponse(user), HttpStatus.OK,
-                            null);
-                } catch (ServiceException e) {
-                    return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
-                }
+            try {
+                UserEntity user = userService.getUserDetail();
+                return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserEntitytoResponse(user), HttpStatus.OK,
+                        null);
+            } catch (ServiceException e) {
+                return ResponseUtil.generateSuccessResponse(null, HttpStatus.NOT_FOUND, e.getMessage());
             }
+        }
 
-            @PostMapping("listing")
-            public ResponseEntity<Object> getUserListing(@RequestBody UserListingRequestDTO userListingRequestDTO) {
-                Integer page = userListingRequestDTO.getPage();
-                Integer pageSize = userListingRequestDTO.getPageSize();
-                String sortBy = userListingRequestDTO.getSortBy();
-                String sortDirection = userListingRequestDTO.getSortDirection();
-                String searchTerm = userListingRequestDTO.getSearchTerm();
-                if (searchTerm == null || searchTerm.isEmpty()) {
-                    return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserPageToUserListingResponseDTO(userService.getUserListingPage(page, pageSize, sortBy, sortDirection)),
+        @PostMapping("listing")
+        public ResponseEntity<Object> getUserListing(@RequestBody UserListingRequestDTO userListingRequestDTO) {
+            Integer page = userListingRequestDTO.getPage();
+            Integer pageSize = userListingRequestDTO.getPageSize();
+            String sortBy = userListingRequestDTO.getSortBy();
+            String sortDirection = userListingRequestDTO.getSortDirection();
+            String searchTerm = userListingRequestDTO.getSearchTerm();
+            if (searchTerm == null || searchTerm.isEmpty()) {
+                return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserPageToUserListingResponseDTO(userService.getUserListingPage(page, pageSize, sortBy, sortDirection)),
+                        HttpStatus.OK,
+                        messageSource.getMessage(MessageConstants.USER_SUCCESS, null, LocaleContextHolder.getLocale()));
+            }
+            return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserPageToUserListingResponseDTO(userService.getUserListingPageWithSearch(page, pageSize, sortBy, sortDirection, searchTerm)),
                             HttpStatus.OK,
                             messageSource.getMessage(MessageConstants.USER_SUCCESS, null, LocaleContextHolder.getLocale()));
-                }
-                return ResponseUtil.generateSuccessResponse(ResponseUtil.mapUserPageToUserListingResponseDTO(userService.getUserListingPageWithSearch(page, pageSize, sortBy, sortDirection, searchTerm)),
-                                HttpStatus.OK,
-                                messageSource.getMessage(MessageConstants.USER_SUCCESS, null, LocaleContextHolder.getLocale()));
-            }
+        }
 
 }
